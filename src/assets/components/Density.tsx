a442d00a4850fccc0e16d430515b50c3e8cfa3f0
Filipp Sam <philippsm666@gmail.com>
2025-06-11 01:18:27 +0300

    add result density

--- src/assets/components/Density.tsx
+++ src/assets/components/Density.tsx
@@ -1,39 +1,56 @@
 import { TextField } from "@mui/material";
 import "../../App.css";
 import { useState, type ChangeEvent } from "react";
+import styles from "./Density.module.css";
 
 export const Density = () => {
   const [data, setData] = useState<string>("");
+  const [dataTemperature, setDataTemperature] = useState<string>("");
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     const value = e.target.value;
-    // Разрешаем: цифры, пробелы и слеши
-    const cleaned = value.replace(/[^0-9 ]/g, "");
+    // Разрешаем: цифры и ..
+    const cleaned = value.replace(/[^0-9,. ]/g, "");
     setData(cleaned);
+  };
+  const handleTemprerature = (e: ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    // Разрешаем: цифры и ..
+    const cleaned = value.replace(/[^0-9,. ]/g, "");
+    setDataTemperature(cleaned);
   };
 
   return (
-    <>
-      <TextField
-        id="outlined-basic"
-        label="Density"
-        variant="outlined"
-        value={data}
-        onChange={handleChange}
-        // Дополнительные параметры для лучшего UX
-        slotProps={{
-          input: {
-            inputMode: "numeric",
-          },
-        }}
-      />
-      {/* <div>Введенное значение: {data}</div> */}
-      <TextField
-        id="outlined-basic"
-        label="Temperature"
-        variant="outlined" 
-        value={data}      
-      />
-    </>
+    <div className={styles.container}>
+      <h4>Расчет плотности по ГОСТ 3900</h4>
+      <div className={styles.inputs}>
+        <TextField
+          label="Density"
+          variant="outlined"
+          value={data}
+          onChange={handleChange}
+          // Дополнительные параметры для лучшего UX
+          slotProps={{
+            input: {
+              inputMode: "numeric",
+            },
+          }}
+        />
+        {/* <div>Введенное значение: {data}</div> */}
+        <TextField
+          label="Temperature"
+          variant="outlined"
+          value={dataTemperature}
+          onChange={handleTemprerature}
+          slotProps={{
+            input: {
+              inputMode: "numeric",
+            },
+          }}
+        />
+      </div>
+
+      <div className={styles.result}>{`Result: ${data + dataTemperature}`}</div>
+    </div>
   );
 };
