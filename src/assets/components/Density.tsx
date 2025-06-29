1a9813217dce4b46372385dff8cef3b2447d1f43
Filipp Sam <philippsm666@gmail.com>
2025-06-30 01:04:25 +0300

    add .prettierrc

--- src/assets/components/Density.tsx
+++ src/assets/components/Density.tsx
@@ -1,105 +1,98 @@
-import {
-  FormControl,
-  InputLabel,
-  MenuItem,
-  Select,
-  TextField,
-  type SelectChangeEvent,
-} from "@mui/material";
-import "../../App.css";
-import { useEffect, useState, type ChangeEvent } from "react";
-import styles from "./Density.module.css";
-import { valuesDensity } from "./bdDensity";
+import { FormControl, InputLabel, MenuItem, Select, TextField, type SelectChangeEvent } from "@mui/material"
+import "../../App.css"
+import { useEffect, useState, type ChangeEvent } from "react"
+import styles from "./Density.module.css"
+import { valuesDensity } from "./bdDensity"
 
 export const Density = () => {
-  const [correction, setCorrection] = useState<string>("0.0014");
-  const [unit, setUnit] = useState<string>("кг/м³");
-  const [data, setData] = useState<string>("");
-  const [dataTemperature, setDataTemperature] = useState<string>("");
-  const [post, setPost] = useState<string | null>(null);
-  const [convertStatus, setConvertStatus] = useState<string>("");
+  const [correction, setCorrection] = useState<string>("0.0014")
+  const [unit, setUnit] = useState<string>("кг/м³")
+  const [data, setData] = useState<string>("")
+  const [dataTemperature, setDataTemperature] = useState<string>("")
+  const [post, setPost] = useState<string | null>(null)
+  const [convertStatus, setConvertStatus] = useState<string>("")
 
   const handleChangeCorrection = (e: SelectChangeEvent<string>) => {
-    setCorrection(e.target.value);
-  };
+    setCorrection(e.target.value)
+  }
 
   const handleChangeUnit = (e: SelectChangeEvent<string>) => {
-    setUnit(e.target.value);
-  };
+    setUnit(e.target.value)
+  }
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const value = e.currentTarget.value;
+    const value = e.currentTarget.value
     // Разрешаем: цифры и ..
-    const cleaned = value.replace(/[^0-9,. ]/g, "");
-    setData(cleaned);
-  };
+    const cleaned = value.replace(/[^0-9,. ]/g, "")
+    setData(cleaned)
+  }
   const handleTemprerature = (e: ChangeEvent<HTMLInputElement>) => {
-    const value = e.currentTarget.value;
+    const value = e.currentTarget.value
     // Разрешаем: цифры и ..
-    const cleaned = value.replace(/[^0-9,. ]/g, "");
-    setDataTemperature(cleaned);
-  };
+    const cleaned = value.replace(/[^0-9,. ]/g, "")
+    setDataTemperature(cleaned)
+  }
 
   const calcDensity20 = () => {
     try {
-      let numDens = Number(data.replace(",", "."));
+      let numDens = Number(data.replace(",", "."))
 
       //для "кг/м3"
       if (unit === "кг/м³" && numDens > 1.11) {
-        numDens = numDens / 1000;
-        setConvertStatus("");
+        numDens = numDens / 1000
+        setConvertStatus("")
       } else if (unit === "кг/м³" && numDens < 1.11) {
-        numDens = numDens;
-        setConvertStatus("density converting in kg/m³");
+        numDens = numDens
+        setConvertStatus("density converting in kg/m³")
       } else if (unit === "г/см³" && numDens > 1.11) {
-        numDens = numDens / 1000;
-        setConvertStatus("density converting in g/cm³");
+        numDens = numDens / 1000
+        setConvertStatus("density converting in g/cm³")
       } else if (unit === "г/см³" && numDens < 1.11) {
-        setConvertStatus("");
+        setConvertStatus("")
       }
 
-      const numTemp = dataTemperature.replace(",", ".");
-      const aroundNumDens = (Math.ceil(numDens * 100) / 100).toFixed(3);
-      const densForTable = String(aroundNumDens).padEnd(6, "0");
-      let tempForTable = numTemp;
+      const numTemp = dataTemperature.replace(",", ".")
+      const aroundNumDens = (Math.ceil(numDens * 100) / 100).toFixed(3)
+      const densForTable = String(aroundNumDens).padEnd(6, "0")
+      let tempForTable = numTemp
 
       if (!numTemp.includes(".")) {
-        tempForTable = tempForTable.padEnd(tempForTable.length + 2, ".0"); // Добавляем ".0"
+        tempForTable = tempForTable.padEnd(tempForTable.length + 2, ".0") // Добавляем ".0"
       }
 
-      const correctionDensity = Number(aroundNumDens) - numDens;
-      const densInTable = valuesDensity[densForTable][tempForTable];
+      const correctionDensity = Number(aroundNumDens) - numDens
+      const densInTable = valuesDensity[densForTable][tempForTable]
 
-      let densityInTable = densInTable - correctionDensity + Number(correction);
+      let densityInTable = densInTable - correctionDensity + Number(correction)
 
       //для "кг/м3"
       if (unit === "кг/м³") {
-        densityInTable = densityInTable * 1000;
-        return densityInTable.toFixed(1);
+        densityInTable = densityInTable * 1000
+        return densityInTable.toFixed(1)
       } else {
-        return densityInTable.toFixed(4);
+        return densityInTable.toFixed(4)
       }
     } catch (error) {
-      console.log("Invalid Syntaxis");
-      setPost("Invalid Syntaxis");
+      console.log("Invalid Syntaxis")
+      setPost("Invalid Syntaxis")
     }
-  };
+  }
 
   useEffect(() => {
     if (data === "" || dataTemperature === "") {
-      setPost(null);
-      return;
+      setPost(null)
+      return
     }
 
     const timer = setTimeout(() => {
-      const density = calcDensity20();
+      const density = calcDensity20()
       if (density) {
-        setPost(density);
+        setPost(density)
       }
-    }, 300);
+    }, 300)
 
-    return () => clearTimeout(timer);
-  }, [data, dataTemperature, correction]);
+    return () => clearTimeout(timer)
+  }, [data, dataTemperature, correction])
 
   return (
     <div className={styles.container}>
@@ -113,12 +106,8 @@ export const Density = () => {
             onChange={handleChangeCorrection} // Обработчик изменения
           >
             <MenuItem value={"0"}>-</MenuItem>
-            <MenuItem value={"0.0014"}>
-              {unit === "кг/м³" ? "1.4" : "0.0014"}
-            </MenuItem>
-            <MenuItem value={"0.0007"}>
-              {unit === "кг/м³" ? "0.7" : "0.0007"}
-            </MenuItem>
+            <MenuItem value={"0.0014"}>{unit === "кг/м³" ? "1.4" : "0.0014"}</MenuItem>
+            <MenuItem value={"0.0007"}>{unit === "кг/м³" ? "0.7" : "0.0007"}</MenuItem>
           </Select>
         </FormControl>
         <FormControl sx={{ minWidth: 70, maxWidth: 100 }}>
@@ -163,10 +152,8 @@ export const Density = () => {
       <div className={styles.result}>
         <p>{convertStatus}</p>
 
-        {`Result: ${post !== null ? post : ""}${
-          post !== null ? ` ${unit === "кг/м³" ? "kg/m³" : "g/cm³"}` : ""
-        }`}
+        {`Result: ${post !== null ? post : ""}${post !== null ? ` ${unit === "кг/м³" ? "kg/m³" : "g/cm³"}` : ""}`}
       </div>
     </div>
-  );
-};
+  )
+}
