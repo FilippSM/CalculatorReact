9defa3043e9ae281886130a0d5309a349e157850
Filipp Sam <philippsm666@gmail.com>
2025-07-14 00:46:00 +0300

    (const header): add nav, add styles for nav

--- src/assets/components/Header.tsx
+++ src/assets/components/Header.tsx
@@ -1,15 +1,38 @@
 import { NavLink } from "react-router"
 import { Path } from "../Route/Routing"
 import { Container } from "./Container/Container"
-
+import styles from "./Header.module.css"
 
 export const Header = () => {
   return (
     <>
       <Container>
-        <NavLink to={Path.Density}>Density</NavLink>
-        <NavLink to={Path.Login}>Login</NavLink>
-        <NavLink to={Path.Main}>Main</NavLink>
+        <nav className={styles.nav}>
+          <NavLink 
+            to={Path.Density}
+            className={({ isActive }) => 
+              isActive ? `${styles.link} ${styles.active}` : styles.link
+            }
+          >
+            Density
+          </NavLink>
+          <NavLink 
+            to={Path.Login}
+            className={({ isActive }) => 
+              isActive ? `${styles.link} ${styles.active}` : styles.link
+            }
+          >
+            Login
+          </NavLink>
+          <NavLink 
+            to={Path.Main}
+            className={({ isActive }) => 
+              isActive ? `${styles.link} ${styles.active}` : styles.link
+            }
+          >
+            Main
+          </NavLink>
+        </nav>
       </Container>
     </>
   )
