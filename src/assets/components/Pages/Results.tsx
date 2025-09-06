5710427b26b47a10cde7de8c7eb314bc48eedf77
Filipp Sam <philippsm666@gmail.com>
2025-09-06 15:50:43 +0300

    add form in results

--- src/assets/components/Pages/Results.tsx
+++ src/assets/components/Pages/Results.tsx
@@ -7,11 +7,21 @@ import {
   useDeleteDensityMutation,
   useGetDensityQuery,
 } from "../../../features/density/api/densityApi"
+import { useForm, type SubmitHandler } from "react-hook-form"
 
 export const Results = () => {
   const { data: densitys20, isLoading } = useGetDensityQuery()
   const [deleteDensity] = useDeleteDensityMutation()
   const [changeDensity] = useChangeDensityMutation()
+
+  const { register, handleSubmit, reset } = useForm<AuthFormData>()
+
+  const onSubmit: SubmitHandler<AuthFormData> = data => {
+      createLogin(data)
+      .then(() => {
+        reset()
+      })
+    }
 
   const handleDelete = (id: number) => {
     if (confirm("Are you sure tou want to delete the density data?")) {
@@ -21,10 +31,10 @@ export const Results = () => {
 
   const handleChange = (id: number, newDensity: string) => {
     changeDensity({
-      id: id,          
-      densityData: {          
-        density: newDensity
-      }
+      id: id,
+      densityData: {
+        density: newDensity,
+      },
     })
   }
 
@@ -38,22 +48,31 @@ export const Results = () => {
           {densitys20?.data?.map((density20: Density20) => {
             return (
               <li key={density20.id} className={styles.resultsList}>
-                {`Плотность при 20 °C: ${density20.densityFor20}`}
-                <Button variant="contained" color="error" onClick={() => handleDelete(density20.id)}>
-                  X
-                </Button>
-                <Button
-                  variant="contained"
-                  sx={{
-                    backgroundColor: "#ff9800",
-                    "&:hover": {
-                      backgroundColor: "#f57c00",
-                    },
-                  }}
-                  onClick={() => handleChange(1, "666666")}
-                >
-                  Change
-                </Button>
+                <div>
+                  {`Плотность при 20 °C: ${density20.densityFor20}`}
+                  <Button variant="contained" color="error" onClick={() => handleDelete(density20.id)}>
+                    X
+                  </Button>
+                  <Button
+                    variant="contained"
+                    sx={{
+                      backgroundColor: "#ff9800",
+                      "&:hover": {
+                        backgroundColor: "#f57c00",
+                      },
+                    }}
+                    onClick={() => handleChange(1, "666666")}
+                  >
+                    Change
+                  </Button>
+                </div>
+                <form onSubmit={handleSubmit(onSubmit)}>
+                  <input {...register("density")} placeholder="density" />
+                  <button type={"submit"}>save</button>
+                  <button type={"button"} onClick={() => {}}>
+                    cancel
+                  </button>
+                </form>
               </li>
             )
           })}
