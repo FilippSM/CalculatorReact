34aad4950950c7c1b4cbf80924545f009356c115
FilippSM_laptop <philippsm666@gmail.com>
2025-09-09 00:21:23 +0300

    (const results) add button save cancel

--- src/assets/components/Pages/Results.tsx
+++ src/assets/components/Pages/Results.tsx
@@ -8,20 +8,30 @@ import {
   useGetDensityQuery,
 } from "../../../features/density/api/densityApi"
 import { useForm, type SubmitHandler } from "react-hook-form"
+import { useState } from "react"
 
 export const Results = () => {
+  const [densityId, setDensityId] = useState<number | null>(null)
+
+
   const { data: densitys20, isLoading } = useGetDensityQuery()
   const [deleteDensity] = useDeleteDensityMutation()
   const [changeDensity] = useChangeDensityMutation()
 
-  const { register, handleSubmit, reset } = useForm<AuthFormData>()
+  const { register, handleSubmit, reset } = useForm<Density20>()
 
-  const onSubmit: SubmitHandler<AuthFormData> = data => {
-      createLogin(data)
-      .then(() => {
-        reset()
-      })
-    }
+  const onSubmit: SubmitHandler<Density20> = data => {
+    if(!densityId) return
+    
+    changeDensity({
+      id: densityId,
+      densityData: {
+        density: data.densityFor20,
+      },
+    }).then(() => {
+      setDensityId(null) //закрытие формы
+    })
+  }
 
   const handleDelete = (id: number) => {
     if (confirm("Are you sure tou want to delete the density data?")) {
@@ -29,15 +39,18 @@ export const Results = () => {
     }
   }
 
-  const handleChange = (id: number, newDensity: string) => {
-    changeDensity({
-      id: id,
-      densityData: {
-        density: newDensity,
-      },
-    })
+  const editHandleChange = (density20: Density20 | null) => {
+    if (density20) {
+      setDensityId(density20.id)
+      reset({
+        densityFor20: density20.densityFor20
+      })
+    } else {
+      setDensityId(null)
+    }
   }
 
+  
   return (
     <Container>
       <h1>Results</h1>
@@ -46,33 +59,37 @@ export const Results = () => {
       ) : (
         <ul>
           {densitys20?.data?.map((density20: Density20) => {
+
+            const isEditing = density20.id === densityId
+
             return (
               <li key={density20.id} className={styles.resultsList}>
-                <div>
-                  {`Плотность при 20 °C: ${density20.densityFor20}`}
-                  <Button variant="contained" color="error" onClick={() => handleDelete(density20.id)}>
-                    X
-                  </Button>
-                  <Button
-                    variant="contained"
-                    sx={{
-                      backgroundColor: "#ff9800",
-                      "&:hover": {
-                        backgroundColor: "#f57c00",
-                      },
-                    }}
-                    onClick={() => handleChange(1, "666666")}
-                  >
-                    Change
-                  </Button>
-                </div>
-                <form onSubmit={handleSubmit(onSubmit)}>
-                  <input {...register("density")} placeholder="density" />
-                  <button type={"submit"}>save</button>
-                  <button type={"button"} onClick={() => {}}>
-                    cancel
-                  </button>
-                </form>
+                {
+                  isEditing ?
+                  <form onSubmit={handleSubmit(onSubmit)} className={styles.resultsForm}>
+                      <input {...register("densityFor20")} placeholder="density" />
+                      <Button type={"submit"} variant="contained" color="success">save</Button>
+                      <Button type={"button"} variant="contained" color="error" onClick={() => editHandleChange(null)}>cancel</Button>
+                  </form> :
+                  <>
+                    {`Плотность при 20 °C: ${density20.densityFor20}`}
+                    <Button variant="contained" color="error" onClick={() => handleDelete(density20.id)}>
+                      X
+                    </Button>
+                    <Button
+                      variant="contained"
+                      sx={{
+                        backgroundColor: "#ff9800",
+                        "&:hover": {
+                          backgroundColor: "#f57c00",
+                        },
+                      }}
+                      onClick={() => editHandleChange(density20)}
+                    >
+                      Change
+                    </Button>
+                  </>
+                }           
               </li>
             )
           })}
