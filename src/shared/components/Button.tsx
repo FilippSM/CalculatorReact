5366ba191fed6e90339820e765486a54a0076834
Filipp Sam <philippsm666@gmail.com>
2025-12-30 01:33:00 +0300

    T-2 feat:  added button in sass

---
+++ src/shared/components/Button.tsx
@@ -0,0 +1,33 @@
+
+import { Slot } from '@radix-ui/react-slot'
+import clsx from 'clsx'
+import styles from './Button.module.scss'
+import type { ComponentPropsWithRef } from 'react'
+
+type Props = {
+  variant?: 'primary' | 'secondary' | 'outlined' | 'textButton'
+  asChild?: boolean
+  fullWidth?: boolean
+} & ComponentPropsWithRef<'button'>
+
+export const Button = ({
+  asChild = false,
+  variant = 'primary',
+  className,
+  fullWidth = false,
+  ...rest
+}: Props) => {
+  const Component = asChild ? Slot : 'button'
+
+  return (
+    <Component
+      className={clsx(
+        styles.button,
+        styles[variant],
+        fullWidth && styles.fullWidth,
+        className
+      )}
+      {...rest}
+    />
+  )
+}
