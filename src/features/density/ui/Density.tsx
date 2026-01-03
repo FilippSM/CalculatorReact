9d061eee21c5dbf04491c5d3df0904e5db9fed00
Filipp Sam <philippsm666@gmail.com>
2026-01-03 12:03:26 +0300

    T-5 feat: added __test__

--- src/features/density/ui/Density.tsx
+++ src/features/density/ui/Density.tsx
@@ -2,11 +2,11 @@ import { Button, FormControl, InputLabel, MenuItem, Select, TextField, type Sele
 import { useEffect, useState, type ChangeEvent } from "react"
 import styles from "./Density.module.css"
 
+import { useDebounce } from "@/assets/utils/useDebounce"
 import { useSaveDensityMutation } from "../api/densityApi"
 import { valuesDensity } from "../bdDensity"
-import { Container } from "../../../assets/components/Container/Container"
-import { SimplePopup } from "./SimplePopup"
-import { useDebounce } from "@/assets/utils/useDebounce"
+import { Container } from "@/assets/components/Container/Container"
+import { SimplePopup } from "../../Popup"
 
 export const Density = () => {
   const [correction, setCorrection] = useState<string>("0.0014")
@@ -120,7 +120,7 @@ export const Density = () => {
   return (
     <Container>
       <div className={styles.container}>
-        <div className={styles['header-dencity']}>
+        <div className={styles["header-dencity"]}>
           <h1>Расчет плотности по ГОСТ 3900 № 1</h1>
           <SimplePopup />
         </div>
@@ -148,7 +148,7 @@ export const Density = () => {
               <MenuItem value={"кг/м³"}>кг/м³</MenuItem>
             </Select>
           </FormControl>
-          <div className={styles['inputs-group']}>
+          <div className={styles["inputs-group"]}>
             <TextField
               label={`Density, ${unit === "кг/м³" ? "kg/m³" : "g/cm³"}`}
               variant="outlined"
